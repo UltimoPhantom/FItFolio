f6194f299c505b87139c406ea0c90f2864d115ad
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-15 02:07:48 +0530

    More design changes

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -16,12 +16,18 @@ const WorkoutDetails = ({ workout }) => {
     }
 
     return (
-        <div className="workout-details" style={{ color: '#f4f9f9', backgroundColor: '#0e1a18', border: '2px solid #59a7a4', padding: '10px', borderRadius: '8px' }}>
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.load}</p>
-        <p><strong>Reps : </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <span onClick={handleClick}>Delete</span>
+        <div className="workout-details" style={{ 
+            color: '#f1ecf1' ,  
+            backgroundColor: '#0e1a18', 
+            border: '2px solid #365559', 
+            padding: '10px', 
+            borderRadius: '8px' 
+        }}>
+            <h4>{workout.title}</h4>
+            <p><strong>Load (kg): </strong>{workout.load}</p>
+            <p><strong>Reps : </strong>{workout.reps}</p>
+            <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
