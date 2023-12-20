591ec7b5536728ecd667ed544ba7e2b660e37824
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-20 22:15:14 +0530

    Implemented Error handling during signup

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -7,8 +7,17 @@ const loginUser = async (req, res) => {
 
 
 //Signup
-const signupUser = async (req, res) => {
-    res.json({mssg: 'Signup user'})
+const signupUser = async function(req, res) {
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+    
 }
 
 module.exports = { loginUser, signupUser }
