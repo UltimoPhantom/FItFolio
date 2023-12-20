f20613528842117a48a9f1b5eb0680debfbc0d77
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-20 21:34:48 +0530

    changes

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema ({
     email: {
@@ -12,5 +13,24 @@ const userSchema = new Schema ({
         required: true,
     }
 })
+
+//Static signup methods
+userSchema.statics.signup = async (email, password) => {
+
+    const exist = await this.findOne({ email })
+
+    if(exist) {
+        throw Error('Email already in use!!')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    console.log("Salt value: ",salt)
+    
+    const hash = await bcrypt.hash(password, salt)
+    console.log("Hash value: ",hash)
+
+    const user = await this.create({ email, password: hash })    
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const workoutRoutes = require('./routes/workout')
-const userRoutes = require('./controllers/userController')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
