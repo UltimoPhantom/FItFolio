2845b4315a4906239b1e7cfa9adc05f4d5bea12d
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2023-12-22 22:00:40 +0530

    Added Lougout (clears local storage, sets user context to null)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+      logout()
+    }
+
     return (
         <header style={{ backgroundColor: '#0e1a18', padding: '3px' }}>
           <div className="container">
@@ -8,6 +15,10 @@ const Navbar = () => {
               <h1 style={{ color: '#f4f9f9' }}>Workout Buddy</h1>
             </Link>
           <nav>
+            <div style={{ color: '#f4f9f9' }}>
+              <button onClick={handleClick}>Logout</button>
+            </div>
+
             <div>
               <Link to='/login' style={{ color: '#f4f9f9' }}>Login</Link>
             </div>
